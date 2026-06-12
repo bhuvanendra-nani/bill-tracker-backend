238ac054f22c7f080d9dec9b3eb49aeb6704bf23
bhuvanendra-nani <bhuvanendranani@gmail.com>
2026-06-12 10:37:29 +0530

    server.js updated

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
+  "http://localhost:4173",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "http://127.0.0.1:5175",
+  "http://127.0.0.1:4173",
   "https://bill-tracker-peach-ten.vercel.app",
 ];
 
@@ -34,6 +36,7 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   optionsSuccessStatus: 204,
+  maxAge: 86400,
 };
 
 app.use(cors(corsOptions));
